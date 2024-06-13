bf39b786193c326de93590c4c7000ab1bac22f6b
Oscar <ruizoki@gmail.com>
2024-06-13 17:32:19 -0600

    Borrado de registros

---
+++ Borrado de registros.sql
@@ -0,0 +1,13 @@
+---------------BORRADO DE REGISTROS DE TRES MANERAS
+
+
+DELETE FROM tb_articulos WHERE codigo_ar IN (5,6);
+
+DELETE FROM tb_articulos WHERE codigo_ar>4;
+
+DELETE FROM tb_articulos WHERE marca_ar='STANDARD'
+
+
+
+VIDEO NUMERO 16
+ 
